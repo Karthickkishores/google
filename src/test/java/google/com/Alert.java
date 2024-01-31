530abeac0f6a55e6fda771868899b35f777f90db
Administrator <Administrator@172.31.4.242>
2024-01-31 07:07:19 +0000

    test

--- src/test/java/google/com/Alert.java
+++ src/test/java/google/com/Alert.java
@@ -1,15 +1,31 @@
 package google.com;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Alert {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	ChromeOptions options = new ChromeOptions();
 	WebDriver driver = new ChromeDriver (options);
-	driver.get("https://demo.automationtesting.in/Alerts.html");
+	driver.get("https://demoqa.com/alerts");
+	Thread.sleep(5000);
+	
+	//driver.findElement(By.linkText("Alert with OK")).click();
+	//driver.findElement(By.linkText("click the button to display an  alert box:")).click();   
+	driver.findElement(By.id("promtButton")).click();
+	
+	org.openqa.selenium.Alert alert = driver.switchTo().alert();
+
+	alert.sendKeys("testing");
+	
+	alert.accept();
+	
+	
+	
+	
 	
 	}
 
