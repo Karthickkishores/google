c0206202f5197cd673715d9f971783634c4c12bf
Administrator <Administrator@172.31.4.242>
2024-01-31 06:04:41 +0000

    test

--- src/test/java/google/com/Alert.java
+++ src/test/java/google/com/Alert.java
@@ -1,10 +1,16 @@
 package google.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Alert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	ChromeOptions options = new ChromeOptions();
+	WebDriver driver = new ChromeDriver (options);
+	driver.get("https://demo.automationtesting.in/Alerts.html");
+	
 	}
 
 }
